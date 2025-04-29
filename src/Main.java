64d8ac92a6de07645455b50b3c93808d3c576074
AstroSW <stdsw@mail.ru>
2025-04-29 15:08:12 +0400

    ДЗ исправлено

--- src/Main.java
+++ src/Main.java
@@ -10,41 +10,4 @@ public class Main {
         System.out.println(lermontov.getFirstName() +" "+ lermontov.getLastName() +", "+ geroyNV.getTitle() +", "+ geroyNV.getYearPublication());
     }
 }
-class Book {
-    private String title;
-    private Author author;
-    private int yearPublication;
 
-    public Book (String title, Author author, int yearPublication) {
-        this.title = title;
-        this.author = author;
-        this.yearPublication = yearPublication;
-    }
-    public String getTitle () {
-        return this.title;
-    }
-    public Author getAuthor () {
-        return this.author;
-    }
-    public int getYearPublication () {
-        return this.yearPublication;
-    }
-    public void setYearPublication (int yearPublication) {
-        this.yearPublication = yearPublication;
-    }
-}
-class Author {
-    private String firstName;
-    private String lastName;
-
-    public Author(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    public String getFirstName () {
-        return this.firstName;
-    }
-    public String getLastName () {
-        return this.lastName;
-    }
-}
